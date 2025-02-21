26b6f4a471dda75f66304c71472e3bbc3df265a9
gabriellauk <17279123+gabriellauk@users.noreply.github.com>
2025-02-21 19:49:56 +0000

    Fix - handle empty body on any status

--- client/src/ApiClient.ts
+++ client/src/ApiClient.ts
@@ -7,9 +7,8 @@ export default class ApiClient {
       query = '?' + query;
     }
 
-    let response;
     try {
-      response = await fetch(BASE_API_URL + options.url + query, {
+      const response = await fetch(BASE_API_URL + options.url + query, {
         method: options.method,
         headers: {
           'Content-Type': 'application/json',
@@ -18,25 +17,26 @@ export default class ApiClient {
         body: options.body ? JSON.stringify(options.body) : null,
         credentials: 'include',
       });
+
+      const bodyText = await response.text();
+      const jsonBody = bodyText ? JSON.parse(bodyText) : null;
+
+      return {
+        ok: response.ok,
+        status: response.status,
+        body: jsonBody,
+      };
     } catch (error: any) {
-      response = {
+      return {
         ok: false,
         status: 500,
-        json: async () => {
-          return {
-            code: 500,
-            message: 'The server is unresponsive',
-            description: error.toString(),
-          };
+        body: {
+          code: 500,
+          message: 'The server is unresponsive',
+          description: error.toString(),
         },
       };
     }
-
-    return {
-      ok: response.ok,
-      status: response.status,
-      body: response.status !== 204 ? await response.json() : null,
-    };
   }
 
   async get(url: string, query?: string, options?: any) {
--- client/src/hooks/useGetUser.tsx
+++ client/src/hooks/useGetUser.tsx
@@ -3,24 +3,18 @@ import { useApi } from '../contexts/ApiProvider';
 
 export function useGetUser() {
   const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isCheckingLoginStatus, setIsLoading] = useState(true);
   const api = useApi();
 
   useEffect(() => {
     const checkUserStatus = async () => {
-      try {
-        const response = await api.get('/user');
-        setUserIsLoggedIn(response.status === 200);
-      } catch (error) {
-        console.error('Could not get user:', error);
-        setUserIsLoggedIn(false);
-      } finally {
-        setIsLoading(false);
-      }
+      const response = await api.get('/user');
+      setUserIsLoggedIn(response.status === 200);
+      setIsLoading(false);
     };
 
     checkUserStatus();
   }, []);
 
-  return { userIsLoggedIn, isLoading };
+  return { userIsLoggedIn, isLoading: isCheckingLoginStatus };
 }
