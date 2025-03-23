9ffca871ecc51c34a13c4e58fe56727abd019630
gabriellauk <17279123+gabriellauk@users.noreply.github.com>
2025-03-23 16:14:31 +0000

    Add Medication table and other DB updates

--- server/app/models.py
+++ server/app/models.py
@@ -6,10 +6,12 @@ from flask_login import UserMixin
 
 from app.extensions import db
 
+from enum import Enum
+
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True, nullable=False)
 
     def __repr__(self):
         return "<User {}>".format(self.email)
@@ -17,14 +19,14 @@ class User(UserMixin, db.Model):
 
 class AnimalType(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    name: so.Mapped[str] = so.mapped_column(sa.String(30), index=True, unique=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(30), unique=True, nullable=False)
 
 
 class Animal(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    name: so.Mapped[str] = so.mapped_column(sa.String(30), index=True)
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
-    animal_type_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(AnimalType.id), index=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(30), nullable=False)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True, nullable=False)
+    animal_type_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(AnimalType.id), index=True, nullable=False)
 
     user = relationship("User")
     animal_type = relationship("AnimalType")
@@ -32,27 +34,60 @@ class Animal(db.Model):
 
 class Symptom(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    description: so.Mapped[str] = so.mapped_column(sa.String(240), index=True)
-    date: so.Mapped[datetime.date] = so.mapped_column(sa.Date, index=True)
-    animal_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Animal.id), index=True)
+    description: so.Mapped[str] = so.mapped_column(sa.String(240), nullable=False)
+    date: so.Mapped[datetime.date] = so.mapped_column(sa.Date, nullable=False)
+    animal_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Animal.id), index=True, nullable=False)
 
     animal = relationship("Animal")
 
 
 class Weight(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    weight: so.Mapped[str] = so.mapped_column(sa.Integer, index=True)
-    date: so.Mapped[datetime.date] = so.mapped_column(sa.Date, index=True)
-    animal_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Animal.id), index=True)
+    weight: so.Mapped[int] = so.mapped_column(sa.Integer, nullable=False)
+    date: so.Mapped[datetime.date] = so.mapped_column(sa.Date, nullable=False)
+    animal_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Animal.id), index=True, nullable=False)
 
     animal = relationship("Animal")
 
 
 class Appointment(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    description: so.Mapped[str] = so.mapped_column(sa.String(240), index=True)
-    date: so.Mapped[datetime.date] = so.mapped_column(sa.Date, index=True)
-    notes: so.Mapped[str] = so.mapped_column(sa.String(750), index=True, nullable=True)
-    animal_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Animal.id), index=True)
+    description: so.Mapped[str] = so.mapped_column(sa.String(240), nullable=False)
+    date: so.Mapped[datetime.date] = so.mapped_column(sa.Date, nullable=False)
+    notes: so.Mapped[str | None] = so.mapped_column(sa.String(750), nullable=True)
+    animal_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Animal.id), index=True, nullable=False)
+
+    animal = relationship("Animal")
+
+
+class TimeUnit(Enum):
+    DAY = "day"
+    WEEK = "week"
+    MONTH = "month"
+    YEAR = "year"
+
+
+def get_enum_values(enum_class):
+    return [member.value for member in enum_class]
+
+
+class Medication(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(30), nullable=False)
+    is_recurring: so.Mapped[bool] = so.mapped_column(sa.Boolean, default=False, nullable=False)
+    times_per_day: so.Mapped[int | None] = so.mapped_column(sa.Integer, nullable=True)
+    frequency_number: so.Mapped[int | None] = so.mapped_column(sa.Integer, nullable=True)
+    frequency_unit: so.Mapped[TimeUnit | None] = so.mapped_column(
+        sa.Enum(TimeUnit, values_callable=get_enum_values), nullable=True
+    )
+    duration_number: so.Mapped[int | None] = so.mapped_column(sa.Integer, nullable=True)
+    duration_unit: so.Mapped[TimeUnit | None] = so.mapped_column(
+        sa.Enum(TimeUnit, values_callable=get_enum_values), nullable=True
+    )
+    start_date: so.Mapped[datetime.date] = so.mapped_column(sa.Date, nullable=False)
+    # TODO: Make end date nullable
+    end_date: so.Mapped[datetime.date] = so.mapped_column(sa.Date, nullable=False)
+    notes: so.Mapped[str | None] = so.mapped_column(sa.String(750), nullable=True)
+    animal_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Animal.id), index=True, nullable=False)
 
     animal = relationship("Animal")
