b388a7773f9c03840923c45daba19ced865fe22a
gabriellauk <17279123+gabriellauk@users.noreply.github.com>
2025-04-19 19:24:45 +0100

    Set animal icon based on species

--- client/src/features/dashboard/components/Profile/Profile.tsx
+++ client/src/features/dashboard/components/Profile/Profile.tsx
@@ -1,15 +1,23 @@
 import { Avatar, Paper, Text } from '@mantine/core';
-import { GiCat } from 'react-icons/gi';
+import { GiCat, GiRabbit, GiSittingDog } from 'react-icons/gi';
 import { useAnimals } from '../../../../contexts/AnimalsContext';
 import styles from './Profile.module.css';
 
 export default function Profile({ weight }: { weight: number | null }) {
   const { animal } = useAnimals();
 
+  const animalIcons: Record<any, JSX.Element> = {
+    1: <GiCat size={80} />,
+    2: <GiSittingDog size={80} />,
+    3: <GiRabbit size={80} />,
+  };
+
+  const icon = animalIcons[animal!.animalTypeId];
+
   return (
     <Paper withBorder className={styles.paper}>
       <Avatar size={120} radius={120} className={styles.avatar}>
-        <GiCat size={80} />
+        {icon}
       </Avatar>
       <Text ta="center" fz="lg" fw={500} mt="md">
         {animal!.name}
